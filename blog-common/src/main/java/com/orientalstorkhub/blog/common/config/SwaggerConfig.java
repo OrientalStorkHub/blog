3271e0b0ffd5d8bc7e54d506c177e6d52b776850
zhangjun <1501790343@qq.com>
2024-08-14 11:19:10 +0800

    docs - introduce openapi3 Knife4j

---
+++ blog-common/src/main/java/com/orientalstorkhub/blog/common/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.orientalstorkhub.blog.common.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Service Name API")
+                .description("API documentation for Service Name")
+                .version("v1.0"));
+    }
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
